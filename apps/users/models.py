d15929d889583b437cd9fff63474e231622bc8ce
CJ Ogbuehi <fahzee1@gmail.com>
2014-01-12 23:32:49 -0600

    test fixing tabs

--- apps/users/models.py
+++ apps/users/models.py
@@ -15,156 +15,156 @@ from apps.challenges.models import Challenge, ChallengeResults, TimeStampedModel
 
 
 class Friends(TimeStampedModel):
-	user = models.ForeignKey(User, related_name='friend_creator_set')
-	friend = models.ForeignKey(User, related_name='friend_set')
-	display_name = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(User, related_name='friend_creator_set')
+    friend = models.ForeignKey(User, related_name='friend_set')
+    display_name = models.CharField(max_length=255, blank=True, null=True)
 
-	def __unicode__(self):
-		return "%s is friends with %s" %(self.friend, self.user)
+    def __unicode__(self):
+        return "%s is friends with %s" %(self.friend, self.user)
 
 
 
 class UserProfile(TimeStampedModel):
-	user = models.OneToOneField(User)
-	score = models.IntegerField(default=0, blank=True, null=True)
-	phone_number = models.CharField(max_length=255, blank=True, null=True)
-	facebook_user = models.BooleanField(default=False , blank=True)
-	last_activity = models.DateTimeField(default=now,auto_now_add=True)
-	device_token = models.CharField(default="",max_length=255)
-	privacy = models.IntegerField(default=0)
-	sent_challenges = models.IntegerField(default=0)
+    user = models.OneToOneField(User)
+    score = models.IntegerField(default=0, blank=True, null=True)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+    facebook_user = models.BooleanField(default=False , blank=True)
+    last_activity = models.DateTimeField(default=now,auto_now_add=True)
+    device_token = models.CharField(default="",max_length=255)
+    privacy = models.IntegerField(default=0)
+    sent_challenges = models.IntegerField(default=0)
 
 
-	def __unicode__(self):
-		return self.user.username
+    def __unicode__(self):
+        return self.user.username
 
-	def return_json(self, login=False):
-		"""
-		creating json response that mirrors that
-		of django tastypie for custom login method
-		"""
-		profile_data = serializers.serialize('json',[self,self.user])
-		p_json = json.loads(profile_data)
-		profile_json = p_json[0]['fields']
-		user_json = p_json[1]['fields']
-		try:
-			del user_json['is_active']
-			del user_json['is_superuser']
-			del user_json['is_staff']
-			del user_json['groups']
-			del user_json['user_permissions']
-			del user_json['password']
-		except KeyError:
-			pass
-
-
-		profile_json['user'] = user_json
-		profile_json['my_friends'] = self.friends
-		profile_json['friend_requests'] = self.friend_requests
-		profile_json['my_challenges'] = self.my_challenges
-		profile_json['received_challenges'] = self.received_challenges
-		profile_json['phone_number'] = base64.b64decode(profile_json['phone_number'])
-
-		if login:
-			profile_json['code'] = 11
-			profile_json['message'] = 'Login was successful.'
-
-		return profile_json
-
-	def my_friends(self):
-		"""
-		Returns json of all users friends
-		"""
-		# or use UserObject.friend_creator_set.filter(user=self)
-		my_list = []
-		blob = {}
-		friends = Friends.objects.filter(user=self.user).select_related()
-		for friend in friends:
-			blob['username'] = friend.friend.username
-			blob['display_name'] = friend.display_name
-			blob['friend_created'] = friend.created_on
-			blob['friends_score'] = friend.friend.userprofile.score
-			blob['friends_last_activity'] = friend.friend.userprofile.last_activity
-			my_list.append(blob)
-
-		return json.dumps(my_list, cls=DjangoJSONEncoder)
-
-	friends = property(my_friends)
-
-	def my_requests(self):
-		"""
-		Returns json of all friends that added
-		this user as a friend
-		"""
-		my_list = []
-		blob = {}
-		friends = Friends.objects.filter(friend=self.user).select_related()
-		for friend in friends:
-			blob['username'] = friend.user.username
-			blob['display_name'] = friend.display_name
-			blob['friend_created'] = friend.created_on
-			blob['friends_score'] = friend.user.userprofile.score
-			blob['friends_last_activity'] = friend.user.userprofile.last_activity
-			my_list.append(blob)
-
-		return json.dumps(my_list, cls=DjangoJSONEncoder)
-
-	friend_requests = property(my_requests)
-
-	def my_challenges(self):
-		"""
-		Returns json of all challenges
-		this user has sent
-		"""
-		
-		my_list = []
-		blob = {'results':[]}
-		challenges = Challenge.objects.filter(sender=self.user).select_related()
-		for challenge in challenges:
-			blob['id'] = challenge.challenge_id
-			blob['media_type'] = challenge.media_type
-			blob['challenge_type'] = challenge.challenge_type
-			blob['challenge_created'] = challenge.created_on
-			blob['name'] = challenge.name
-			for result in challenge.results.all():
-				result_blob = {'player': result.player.username,
-				               'success': result.success}
-				blob['results'].append(result_blob)
-
-			my_list.append(blob)
-
-		return json.dumps(my_list, cls=DjangoJSONEncoder)
+    def return_json(self, login=False):
+        """
+        creating json response that mirrors that
+        of django tastypie for custom login method
+        """
+        profile_data = serializers.serialize('json',[self,self.user])
+        p_json = json.loads(profile_data)
+        profile_json = p_json[0]['fields']
+        user_json = p_json[1]['fields']
+        try:
+            del user_json['is_active']
+            del user_json['is_superuser']
+            del user_json['is_staff']
+            del user_json['groups']
+            del user_json['user_permissions']
+            del user_json['password']
+        except KeyError:
+            pass
 
 
-	my_challenges = property(my_challenges)
+        profile_json['user'] = user_json
+        profile_json['my_friends'] = self.friends
+        profile_json['friend_requests'] = self.friend_requests
+        profile_json['my_challenges'] = self.my_challenges
+        profile_json['received_challenges'] = self.received_challenges
+        profile_json['phone_number'] = base64.b64decode(profile_json['phone_number'])
+
+        if login:
+            profile_json['code'] = 11
+            profile_json['message'] = 'Login was successful.'
+
+        return profile_json
+
+    def my_friends(self):
+        """
+        Returns json of all users friends
+        """
+        # or use UserObject.friend_creator_set.filter(user=self)
+        my_list = []
+        blob = {}
+        friends = Friends.objects.filter(user=self.user).select_related()
+        for friend in friends:
+            blob['username'] = friend.friend.username
+            blob['display_name'] = friend.display_name
+            blob['friend_created'] = friend.created_on
+            blob['friends_score'] = friend.friend.userprofile.score
+            blob['friends_last_activity'] = friend.friend.userprofile.last_activity
+            my_list.append(blob)
+
+        return json.dumps(my_list, cls=DjangoJSONEncoder)
+
+    friends = property(my_friends)
+
+    def my_requests(self):
+        """
+        Returns json of all friends that added
+        this user as a friend
+        """
+        my_list = []
+        blob = {}
+        friends = Friends.objects.filter(friend=self.user).select_related()
+        for friend in friends:
+            blob['username'] = friend.user.username
+            blob['display_name'] = friend.display_name
+            blob['friend_created'] = friend.created_on
+            blob['friends_score'] = friend.user.userprofile.score
+            blob['friends_last_activity'] = friend.user.userprofile.last_activity
+            my_list.append(blob)
+
+        return json.dumps(my_list, cls=DjangoJSONEncoder)
+
+    friend_requests = property(my_requests)
+
+    def my_challenges(self):
+        """
+        Returns json of all challenges
+        this user has sent
+        """
+        
+        my_list = []
+        blob = {'results':[]}
+        challenges = Challenge.objects.filter(sender=self.user).select_related()
+        for challenge in challenges:
+            blob['id'] = challenge.challenge_id
+            blob['media_type'] = challenge.media_type
+            blob['challenge_type'] = challenge.challenge_type
+            blob['challenge_created'] = challenge.created_on
+            blob['name'] = challenge.name
+            for result in challenge.results.all():
+                result_blob = {'player': result.player.username,
+                               'success': result.success}
+                blob['results'].append(result_blob)
+
+            my_list.append(blob)
+
+        return json.dumps(my_list, cls=DjangoJSONEncoder)
 
 
-	def received_challenges(self):
-		"""
-		Returns json of all challenges
-		this user has received
-		"""
-		my_list = []
-		challenges = ChallengeResults.objects.filter(player=self.user).select_related()
-		for challenge in challenges:
-			blob = {
-		   'challenge':{
-		      'id': challenge.challenge.challenge_id,
-			  'media_type': challenge.challenge.media_type,
-			  'challenge_type': challenge.challenge.challenge_type,
-		      'challenge_created': challenge.challenge.created_on,
-			  'name': challenge.challenge.name,
-			  'result':{
-			   	     'success': challenge.success,
-			         'sender': challenge.challenge.sender.username}
-				          }
-				}
-			my_list.append(blob)
-
-		return json.dumps(my_list, cls=DjangoJSONEncoder)
+    my_challenges = property(my_challenges)
 
 
-	received_challenges = property(received_challenges)
+    def received_challenges(self):
+        """
+        Returns json of all challenges
+        this user has received
+        """
+        my_list = []
+        challenges = ChallengeResults.objects.filter(player=self.user).select_related()
+        for challenge in challenges:
+            blob = {
+           'challenge':{
+              'id': challenge.challenge.challenge_id,
+              'media_type': challenge.challenge.media_type,
+              'challenge_type': challenge.challenge.challenge_type,
+              'challenge_created': challenge.challenge.created_on,
+              'name': challenge.challenge.name,
+              'result':{
+                     'success': challenge.success,
+                     'sender': challenge.challenge.sender.username}
+                          }
+                }
+            my_list.append(blob)
+
+        return json.dumps(my_list, cls=DjangoJSONEncoder)
+
+
+    received_challenges = property(received_challenges)
 
 
 
@@ -175,8 +175,8 @@ class UserProfile(TimeStampedModel):
 
 
 def create_profile(sender,**kwargs):
-	if kwargs['created']:
-		UserProfile.objects.create(user=kwargs['instance'])
+    if kwargs['created']:
+        UserProfile.objects.create(user=kwargs['instance'])
 
 
 """
@@ -199,15 +199,15 @@ REGEX_VALID_PASSWORD = (
     ## check for special characters. Something which is not word, digit or
     ## space will be treated as special character
     #'(?=.*[^\w\d\s])).'
-	')'
+    ')'
     ## Minimum 5 characters
     '{' + str(MINIMUM_PASSWORD_LENGTH) + ',}$')
  
  
 def validate_password(password):
-	if re.match(REGEX_VALID_PASSWORD, password):
-		return True
-	return False
+    if re.match(REGEX_VALID_PASSWORD, password):
+        return True
+    return False
 
 
 
