ecb843e83e93b7ac712dca172015b29b2d1bd713
CJ Ogbuehi <fahzee1@gmail.com>
2014-01-12 23:39:39 -0600

    test fixing tabs

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -5,64 +5,64 @@ from django.contrib.auth.models import User
 
 
 def get_upload_path(instance , filname):
-	path = None
-	if instance.media_type == 0:
-		path = 'photos/%Y/%m/%d'
-	if instance.media_type == 1:
-		path = 'videos/%Y/%m/%d'
-	return path 
+    path = None
+    if instance.media_type == 0:
+        path = 'photos/%Y/%m/%d'
+    if instance.media_type == 1:
+        path = 'videos/%Y/%m/%d'
+    return path 
 
 
 class TimeStampedModel(models.Model):
-	created_on = models.DateTimeField(default=now,auto_now_add=True)
+    created_on = models.DateTimeField(default=now,auto_now_add=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
 
 
 class Challenge(TimeStampedModel):
-	sender = models.ForeignKey(User, blank=False, null=False)
-	name = models.CharField(default='level 1', max_length=255,blank=True, null=True)
-	challenge_id = models.CharField(max_length=255, blank=False, null=False)
-	challenge_media = models.FileField(upload_to=get_upload_path, blank=False, null=False)
-	media_type = models.IntegerField(default=0, help_text='Picture or video?')
-	challenge_type = models.IntegerField(default=0, help_text='What level challenge?')
-	answer = models.CharField(max_length=255, blank=False, null=False)
-	hint = models.CharField(max_length=255, blank=True, null=True)
+    sender = models.ForeignKey(User, blank=False, null=False)
+    name = models.CharField(default='level 1', max_length=255,blank=True, null=True)
+    challenge_id = models.CharField(max_length=255, blank=False, null=False)
+    challenge_media = models.FileField(upload_to=get_upload_path, blank=False, null=False)
+    media_type = models.IntegerField(default=0, help_text='Picture or video?')
+    challenge_type = models.IntegerField(default=0, help_text='What level challenge?')
+    answer = models.CharField(max_length=255, blank=False, null=False)
+    hint = models.CharField(max_length=255, blank=True, null=True)
 
 
-	def __unicode__(self):
-		return "Challenge: %d User: %s" % (self.id, self.sender.username)
+    def __unicode__(self):
+        return "Challenge: %d User: %s" % (self.id, self.sender.username)
 
-	def get_points(self):
-		if self.challenge_type == 0:
-			return 5
-		if self.challenge_type == 1:
-			return 15
-		if self.challenge_type == 2:
-			return 25
+    def get_points(self):
+        if self.challenge_type == 0:
+            return 5
+        if self.challenge_type == 1:
+            return 15
+        if self.challenge_type == 2:
+            return 25
 
 
 class ChallengeResults(TimeStampedModel):
-	challenge = models.ForeignKey(Challenge, blank=False, null=False, related_name='results')
-	player = models.ForeignKey(User, blank=False, null=False)
-	success = models.BooleanField(default=False)
+    challenge = models.ForeignKey(Challenge, blank=False, null=False, related_name='results')
+    player = models.ForeignKey(User, blank=False, null=False)
+    success = models.BooleanField(default=False)
 
 
-	def __unicode__(self):
-		return "%s played challenge '%s' by %s" %(self.player.username, 
-												  self.challenge.name, 
-												  self.challenge.sender.username)
+    def __unicode__(self):
+        return "%s played challenge '%s' by %s" %(self.player.username, 
+                                                  self.challenge.name, 
+                                                  self.challenge.sender.username)
 
 
 class ChallengeSend(TimeStampedModel):
-	sender = models.ForeignKey(User, blank=False, null=False, related_name='sent_from')
-	recipients = models.ManyToManyField(User, blank=False, null=False)
-	challenge = models.ForeignKey(Challenge, blank=False, null=False)
-	retry = models.BooleanField(default=False)
+    sender = models.ForeignKey(User, blank=False, null=False, related_name='sent_from')
+    recipients = models.ManyToManyField(User, blank=False, null=False)
+    challenge = models.ForeignKey(Challenge, blank=False, null=False)
+    retry = models.BooleanField(default=False)
 
-	def __unicode__(self):
-		return "Sent %s's challenge" % self.sender.username
+    def __unicode__(self):
+        return "Sent %s's challenge" % self.sender.username
 
 
--- apps/users/exceptions.py
+++ apps/users/exceptions.py
@@ -5,16 +5,19 @@ from tastypie.http import HttpBadRequest , HttpApplicationError
 
 class CustomBadRequest(TastypieError):
 
-	def __init__(self, code="", message="", my_error=False):
-		self.application_error = my_error
-		self._response = { 
-					 "code": code or "not_provided",
-					 "message": message or "No error message was provided"
-					 }
+    def __init__(self, code="", message="", my_error=False):
+        self.application_error = my_error
+        self._response = { 
+                    "code": code or "not_provided",
+                    "message": message or "No error message was provided"
+                     }
 
-	@property 
-	def response(self):
-		if self.application_error:
-			return HttpApplicationError(json.dumps(self._response), content_type="application/json")
-		else:
-			return HttpBadRequest(json.dumps(self._response),content_type="application/json")
+    @property 
+    def response(self):
+        if self.application_error:
+            return HttpApplicationError(json.dumps(self._response), content_type="application/json")
+        else:
+            return HttpBadRequest(json.dumps(self._response),content_type="application/json")
+
+
+
